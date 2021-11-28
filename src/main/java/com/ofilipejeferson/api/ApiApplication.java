bffccb1e39b0f15773cd4e3b0db00fa349798d37
Filipe Jeferson da Silva <o_filipejeferson@outlook.com>
2021-11-27 23:45:26 -0300

    Initial commit

---
+++ src/main/java/com/ofilipejeferson/api/ApiApplication.java
@@ -0,0 +1,20 @@
+package com.ofilipejeferson.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder Application){
+		return Application.sources(ApiApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
