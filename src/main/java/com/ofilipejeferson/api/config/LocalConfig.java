7170d845477d1645bcd8bf13c66f0a6d0c514d13
Filipe Jeferson da Silva <o_filipejeferson@outlook.com>
2021-11-28 18:13:54 -0300

    perfil de teste

--- src/main/java/com/ofilipejeferson/api/config/LocalConfig.java
+++ src/main/java/com/ofilipejeferson/api/config/LocalConfig.java
@@ -13,13 +13,16 @@ import java.util.List;
 @Profile("local")
 public class LocalConfig {
 
+    @Autowired
     private UserRepository userRepository;
-    
+
     @Bean
         public void startDB(){
         User user1 =  new User(1, "Filipe Jeferson", "filipe@email.com","teste123");
         User user2 =  new User(2, "Luana Cabral", "luana@email.com","teste123");
+        User user3 =  new User(3, "Heitor Miranda", "heitor@email.com","teste123");
+        User user4 =  new User(4, "Noah Miranda", "noah@email.com","teste123");
 
-        userRepository.saveAll(List.of(user1, user2));
+        userRepository.saveAll(List.of(user1, user2, user3, user4));
     }
 }
