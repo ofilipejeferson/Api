07ea397d49581cb7b9546edad91103f58443c521
Filipe Jeferson da Silva <o_filipejeferson@outlook.com>
2021-11-28 00:12:10 -0300

    Classe Usuário adicionada

---
+++ src/main/java/com/ofilipejeferson/api/domain/User.java
@@ -0,0 +1,27 @@
+package com.ofilipejeferson.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+
+}
