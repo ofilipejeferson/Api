74f4019e337ad31d7d240519e92b1fa5316dd2be
Filipe Jeferson da Silva <o_filipejeferson@outlook.com>
2021-11-30 13:52:06 -0300

    Teste para a classe Update com sucesso.

--- src/test/java/com/ofilipejeferson/api/service/impl/UserServiceImplTest.java
+++ src/test/java/com/ofilipejeferson/api/service/impl/UserServiceImplTest.java
@@ -119,7 +119,17 @@ class UserServiceImplTest {
 
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(userRepository.save(any())).thenReturn(user);
+
+        User response = userService.update(userDto);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
