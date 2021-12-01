fa2058378ca9b66b17b2dddeb50679285037c3da
Filipe Jeferson da Silva <o_filipejeferson@outlook.com>
2021-12-01 15:04:01 -0300

    Classe de teste da classe controller do metodo delete().

--- src/test/java/com/ofilipejeferson/api/controller/UserRestControllerTest.java
+++ src/test/java/com/ofilipejeferson/api/controller/UserRestControllerTest.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserRestControllerTest {
@@ -114,7 +114,16 @@ class UserRestControllerTest {
     }
 
     @Test
-    void delete() {
+    void whenDeleteThenReturnSuccess() {
+        doNothing().when(userService).delete(anyInt());
+
+        ResponseEntity<UserDto> response = userRestController.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        verify(userService, times(1)).delete(anyInt());
+
     }
 
     private void startUser(){
